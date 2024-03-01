3d8b189f8440b0867d93fe4e3d870d74800fe8e5
Rene Souza <rene.souza@gmail.com>
2024-03-01 13:30:11 -0300

    fix: removing unused code from home

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import AppRouter from './routes';
-import routes from './routes/config';
-import { Link } from 'react-router-dom';
+
 const App = () => {
 	  return (
 		<>
